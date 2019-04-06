cc51bf66097f1f09495460dab03757c7eb74ba59
Chisatooo <donsimon5@icloud.com>
2019-04-06 15:01:35 +0900

    あああ

--- WordList/AppDelegate.swift
+++ WordList/AppDelegate.swift
@@ -6,13 +6,12 @@
 //  Copyright © 2019 litech. All rights reserved.
 //
 
-import UIKit
-
+  import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- WordList/ViewController.swift
+++ WordList/ViewController.swift
@@ -6,13 +6,17 @@
 //  Copyright © 2019 litech. All rights reserved.
 //
 
+
+
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+          // Do any additional setup after loading the view, typically from a nib.
     }
 
 
